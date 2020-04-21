42d9b1258b2923119489c404c4f15acaa726f22e
MelquiasRibeiro <melquias10.mr@gmail.com>
2020-04-21 20:34:01 -0300

    feat: Config styled-componets

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { Title } from './styles';
 
 export default class Main extends Component {
   render() {
-    return <h1> Main</h1>;
+    return <Title> Main</Title>;
   }
 }
